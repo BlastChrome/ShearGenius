7c6258e66af1163d54221a469f06878396960af8
Nicholas Jackson <nicholasjackson@nj-mini-2014.local>
2021-12-11 11:25:07 -0800

    navbar coming along

--- js/navbar.js
+++ js/navbar.js
@@ -1,30 +1,32 @@
 document.onload = function(){
-const navbtn = document.getElementsByClassName("mobile-nav-btn")[0];  
+
+const navbtn = document.getElementsByClassName("mobile-nav-btn")[0];   
+const mobile_nav = document.getElementsByClassName("mobile-nav-container")[0];
 const header = document.getElementsByClassName("header")[0];  
 let sticky = header.offsetTop;  
-
-let stickyNav = () => { 
-    console.log(window.pageYOffset); 
-    console.log(sticky);
+let navOpacity = () => { 
     if (window.pageYOffset > sticky) 
     {
-        header.classList.add("opacity-on");
-        header.classList.remove("opacity-off");
-      } 
-      else if(window.pageYOffset == sticky) 
-      {
-         header.classList.remove("opacity-on");
+        header.classList.remove("opacity-on");
          header.classList.add("opacity-off");
+      } 
+      else if(window.pageYOffset == 0) 
+      {
+        header.classList.toggle("opacity-on");
+        header.classList.remove("opacity-off");
       } 
 
 }
 
 // handles the hamburger opening/closing animation
-navbtn.addEventListener('click', () => navbtn.classList.toggle('change'));
+navbtn.addEventListener('click', () => { 
+    navbtn.classList.toggle('change'); 
+    mobile_nav.classList.toggle('opacity-full-on');
 
-window.onscroll = () => {
-    stickyNav();
-};
+});
+
+// changes the opacity of the navbar depending on scroll position
+window.onscroll = () => navOpacity();
 
 
 }();
