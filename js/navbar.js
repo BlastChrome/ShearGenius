846b246b98530fec55ad37143d16bf141bdb39aa
Nicholas Jackson <nicholasjackson@nj-mini-2014.local>
2021-12-11 08:57:54 -0800

    git the sticky nav behavior

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,30 @@
 document.onload = function(){
-const navbtn = document.getElementsByClassName("mobile-nav-btn")[0]; 
+const navbtn = document.getElementsByClassName("mobile-nav-btn")[0];  
+const header = document.getElementsByClassName("header")[0];  
+let sticky = header.offsetTop;  
+
+let stickyNav = () => { 
+    console.log(window.pageYOffset); 
+    console.log(sticky);
+    if (window.pageYOffset > sticky) 
+    {
+        header.classList.add("opacity-on");
+        header.classList.remove("opacity-off");
+      } 
+      else if(window.pageYOffset == sticky) 
+      {
+         header.classList.remove("opacity-on");
+         header.classList.add("opacity-off");
+      } 
+
+}
 
 // handles the hamburger opening/closing animation
 navbtn.addEventListener('click', () => navbtn.classList.toggle('change'));
-    
+
+window.onscroll = () => {
+    stickyNav();
+};
+
+
 }();
