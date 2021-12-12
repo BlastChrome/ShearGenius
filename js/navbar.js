798484c62740ae07deb28132f61faf7d5ca56853
Nicholas Jackson <nicholasjackson@nj-mini-2014.local>
2021-12-11 21:18:29 -0800

    further progress with nav, mult glitches...

--- js/navbar.js
+++ js/navbar.js
@@ -2,26 +2,46 @@ document.onload = function(){
 
 const navbtn = document.getElementsByClassName("mobile-nav-btn")[0];   
 const mobile_nav = document.getElementsByClassName("mobile-nav-container")[0];
+let mobile_nav_open = false;
+let scrollLock = true;
 const header = document.getElementsByClassName("header")[0];  
 let sticky = header.offsetTop;  
+
 let navOpacity = () => { 
     if (window.pageYOffset > sticky) 
     {
+        console.log(sticky);
         header.classList.remove("opacity-on");
-         header.classList.add("opacity-off");
-      } 
-      else if(window.pageYOffset == 0) 
-      {
+        header.classList.add("opacity-off");
+    } 
+    else if(window.pageYOffset == 0) 
+    {
+        console.log(sticky);
         header.classList.toggle("opacity-on");
         header.classList.remove("opacity-off");
       } 
 
+} 
+
+let mbNavDisplay = () =>{
+    if(mobile_nav_open){
+        mobile_nav.classList.remove("opacity-full-off"); 
+        mobile_nav.classList.add("opacity-full-on");
+        mobile_nav_open = false;
+    } else{ 
+        mobile_nav.classList.add("opacity-full-off"); 
+        mobile_nav.classList.remove("opacity-full-on");
+        mobile_nav_open = true;         
+    }
 }
 
 // handles the hamburger opening/closing animation
 navbtn.addEventListener('click', () => { 
-    navbtn.classList.toggle('change'); 
-    mobile_nav.classList.toggle('opacity-full-on');
+    
+    
+    navbtn.classList.toggle('change');   
+    mbNavDisplay();
+    // mobile_nav.classList.toggle('opacity-full-on');
 
 });
 
