d03c925f08e278fcd7392e517ca678cfe3ab2759
Nick <nicholasjackson5656@gmail.com>
2022-01-16 17:36:10 -0800

    updated on page SEO for all pages

--- js/map.js
+++ js/map.js
@@ -1,16 +1,1 @@
-function initMap() {
-    // The location of Uluru
-    const sheargenius = { lat: 33.905724, lng: -118.352737 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 18,
-      center: sheargenius,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: sheargenius,
-      map: map,
-    });
-  }; 
-
-
+function initMap(){const e={lat:33.905724,lng:-118.352737},n=new google.maps.Map(document.getElementById("map"),{zoom:18,center:e});new google.maps.Marker({position:e,map:n})}
